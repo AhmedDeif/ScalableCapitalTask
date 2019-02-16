5fad5cf3d8c4dff991ece4a2c5e12e7e54dca2a3
Doola <ahmed.adel.abodeif@gmail.com>
2019-02-16 17:16:36 +0200

    creating project structure

---
+++ ScalableCapitalTaskAbodeif/Utils/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  ScalableCapitalTaskAbodeif
+//
+//  Created by Ahmed Abodeif on 2/16/19.
+//  Copyright © 2019 Ahmed Abodeif. All rights reserved.
+//
+
+import Foundation
+
+struct Constants {
+    
+}
